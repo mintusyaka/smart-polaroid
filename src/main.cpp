5d9b3d2bf3a19f607cb71538241c1bc615efad6a
mintusyaka <sts.mnts@gmail.com>
2026-07-01 15:13:02 +0300

    UPD: add OLED init + simple shows text on display

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #define LED_PIN 38   // або 48, залежно від того, що спрацювало у Фазі 1
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
 
 const char* WIFI_SSID = "Wokwi-GUEST";
 const char* WIFI_PASS = "";
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void connectWiFi() {
   Serial.print("Connecting to WiFi");
@@ -20,11 +30,32 @@ void connectWiFi() {
   Serial.println(WiFi.localIP());
 }
 
+void showText(const String& text) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(text);
+  display.display();
+}
+
+void initOLED() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("SSD1306 allocation failed");
+  } else {
+    showText("Booting...");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Boot OK");
 
+  initOLED();
+
   connectWiFi();
+
+  showText("WiFi Connected\nIP: " + WiFi.localIP().toString());
 }
 
 void loop() {
