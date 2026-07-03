386f250238f754ad3b5a180820f8bc4fe72ade9a
mintusyaka <sts.mnts@gmail.com>
2026-07-03 17:06:16 +0300

    FIX: hide secrets

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include "modules/SmartPolaroidApp.hpp"
+#include "modules/include/secrets.h"
 
 #define LED_PIN 38   // або 48, залежно від того, що спрацювало у Фазі 1
 #define BUTTON_PIN 4
 
-const char* _telegramBotToken = "8952161732:AAGLJmVqU3yzFHrZn2RvTfjk79LKeiQSWEw";
-const char* _telegramChatId = "801685009";
-
-SmartPolaroidApp app(BUTTON_PIN, LED_PIN, _telegramBotToken, _telegramChatId);
+SmartPolaroidApp app(BUTTON_PIN, LED_PIN, BOT_TOKEN, CHAT_ID);
 
 void setup() {
   app.setup();
