5bb6a7e00e209663375513c8742f883031128294
mintusyaka <sts.mnts@gmail.com>
2026-07-03 17:03:35 +0300

    ADD: Telegram Bot connection

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #define LED_PIN 38   // або 48, залежно від того, що спрацювало у Фазі 1
 #define BUTTON_PIN 4
 
-SmartPolaroidApp app(BUTTON_PIN, LED_PIN);
+const char* _telegramBotToken = "8952161732:AAGLJmVqU3yzFHrZn2RvTfjk79LKeiQSWEw";
+const char* _telegramChatId = "801685009";
+
+SmartPolaroidApp app(BUTTON_PIN, LED_PIN, _telegramBotToken, _telegramChatId);
 
 void setup() {
   app.setup();
